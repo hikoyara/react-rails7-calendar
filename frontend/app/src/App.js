0373de6b17578393420483d4768efee0a2b47690
LongTown <longtown@LongTownnoMac-mini-2.local>
2023-08-22 11:43:16 +0900

    login front

--- frontend/app/src/App.js
+++ frontend/app/src/App.js
@@ -1,10 +1,35 @@
 import { Link } from "react-router-dom";
+import { Box, Flex, Image, Text, Input, Button, Link as CLink } from "@chakra-ui/react";
 
 function App() {
     return (
-        <div>
-            <Link to="/signUp">登録画面</Link>
-        </div>
+        <Flex h={"100vh"}>
+            {/* 画像 */}
+            <Flex w="50%" justifyContent="center" alignItems="center" flexDirection="column">
+                <Image w="400px" src="calendar.png" />
+                <Text fontSize="32px" color="blue.500" fontWeight="bold">
+                    Googleカレンダークローンアプリ
+                </Text>
+            </Flex>
+            {/* フォーム */}
+            <Flex w="50%" justifyContent="center" alignItems="center" flexDirection="column">
+                <Box w="400px">
+                    <Text fontSize="24px" color="gray.700" fontWeight="bold" mb="24px">
+                        ログインページ
+                    </Text>
+                    <Input placeholder="メールアドレス" mb="16px" />
+                    <Input placeholder="パスワード" mb="16px" />
+                    <Button w="400px" colorScheme="blue" mb="8px">
+                        ログインする
+                    </Button>
+                    <Box textAlign="right">
+                        <CLink color="blue.500">
+                            <Link to="/signUp">ユーザー登録はこちら</Link>
+                        </CLink>
+                    </Box>
+                </Box>
+            </Flex>
+        </Flex>
     );
 }
 
