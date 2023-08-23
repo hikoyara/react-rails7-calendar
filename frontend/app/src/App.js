f662ed396ba9d63076b719b51d66d7a451154725
LongTown <longtown@LongTownnoMac-mini-2.local>
2023-08-23 13:50:26 +0900

    login backend and signUp

--- frontend/app/src/App.js
+++ frontend/app/src/App.js
@@ -1,7 +1,40 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { useState, useEffect } from "react";
 import { Box, Flex, Image, Text, Input, Button, Link as CLink } from "@chakra-ui/react";
+import { signIn, getUser } from "./lib/api/auth";
+import Cookies from "js-cookie";
 
 function App() {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const navigate = useNavigate();
+
+    const login = async () => {
+        try {
+            const res = await signIn({ email, password });
+            Cookies.set("_access_token", res.headers["access-token"]);
+            Cookies.set("_client", res.headers["client"]);
+            Cookies.set("_uid", res.headers["uid"]);
+            navigate("calendar");
+        } catch (e) {
+            console.log(e);
+        }
+    };
+
+    useEffect(() => {
+        const f = async () => {
+            try {
+                const res = await getUser();
+                if (res.data.isLogin) {
+                    navigate("calendar");
+                }
+            } catch (e) {
+                console.log(e);
+            }
+        };
+        f();
+    }, [navigate]);
+
     return (
         <Flex h={"100vh"}>
             {/* 画像 */}
@@ -17,9 +50,9 @@ function App() {
                     <Text fontSize="24px" color="gray.700" fontWeight="bold" mb="24px">
                         ログインページ
                     </Text>
-                    <Input placeholder="メールアドレス" mb="16px" />
-                    <Input placeholder="パスワード" mb="16px" />
-                    <Button w="400px" colorScheme="blue" mb="8px">
+                    <Input placeholder="メールアドレス" mb="16px" value={email} onChange={(event) => setEmail(event.target.value)} />
+                    <Input placeholder="パスワード" mb="16px" value={password} onChange={(event) => setPassword(event.target.value)} />
+                    <Button w="400px" colorScheme="blue" mb="8px" onClick={login}>
                         ログインする
                     </Button>
                     <Box textAlign="right">
